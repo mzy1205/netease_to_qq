d52206beb30360018f7b81ef0003d027b3e404d6
mzy1205 <872883972@qq.com>
2018-06-07 20:43:00 +0800

    pdate

--- test.py
+++ test.py
@@ -2,19 +2,24 @@ from selenium import webdriver
 import os
 import time
 # from config import cookies
-from pyvirtualdisplay import Display
+# from pyvirtualdisplay import Display
+
+# from xvfbwrapper import Xvfb
+
+# vdisplay = Xvfb()
+# vdisplay.start()
 
 # display = Display(visible=0, size=(800, 800))
 # display.start()
 
-cookie_dir= r'/home/itzj00100/.config/google-chrome'    # 对应你的chrome的用户数据存放路径  
+cookie_dir= r'C:\Users\mzy\AppData\Local\Google\Chrome\User Data'    # 对应你的chrome的用户数据存放路径  
 chrome_options=webdriver.ChromeOptions()
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-setuid-sandbox")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-setuid-sandbox")
 chrome_options.add_argument('--profile-directory=Default')
-chrome_options.add_argument("--user-data-dir="+os.path.abspath(cookie_dir))  
+# chrome_options.add_argument("--user-data-dir="+os.path.abspath(cookie_dir))  
 
-driver=webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+driver=webdriver.Chrome(chrome_options=chrome_options)
 driver.maximize_window()
 driver.get("https://www.baidu.com/")
 
