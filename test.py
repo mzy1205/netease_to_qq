189ad2d1ebab61e292d913516a38b43e6d062ba6
mzy1205 <872883972@qq.com>
2018-06-07 14:48:37 +0800

    test

---
+++ test.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import os
+import time
+# from config import cookies
+from pyvirtualdisplay import Display
+
+# display = Display(visible=0, size=(800, 800))
+# display.start()
+
+cookie_dir= r'/home/itzj00100/.config/google-chrome'    # 对应你的chrome的用户数据存放路径  
+chrome_options=webdriver.ChromeOptions()
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-setuid-sandbox")
+chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_argument("--user-data-dir="+os.path.abspath(cookie_dir))  
+
+driver=webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+driver.maximize_window()
+driver.get("https://www.baidu.com/")
+
+# driver.delete_all_cookies()
+# for k in cookies:
+#     print(k + ' -- ' + str(cookies[k]))
+#     driver.add_cookie({'name':k,'value':cookies[k]})
+
+# driver.get("https://www.baidu.com/")
