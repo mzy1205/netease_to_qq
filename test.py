0afcc5be962bd4ed7c453b023a1120cf26602927
mzy1205 <872883972@qq.com>
2018-06-09 15:03:52 +0800

    update

--- test.py
+++ test.py
@@ -14,10 +14,10 @@ import time
 
 cookie_dir= r'C:\Users\mzy\AppData\Local\Google\Chrome\User Data'    # 对应你的chrome的用户数据存放路径  
 chrome_options=webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-setuid-sandbox")
-chrome_options.add_argument('--profile-directory=Default')
-# chrome_options.add_argument("--user-data-dir="+os.path.abspath(cookie_dir))  
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-setuid-sandbox")
+# chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_argument("user-data-dir="+os.path.abspath(cookie_dir))  
 
 driver=webdriver.Chrome(chrome_options=chrome_options)
 driver.maximize_window()
